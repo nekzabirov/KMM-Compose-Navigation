ffd3c98a4da817e498bbdd9c7cb998eccb26fa60
nekbakhtzabirov <nekbakht.zabirov@gmail.com>
2023-07-24 01:43:34 +0300

    Clear code

--- src/commonMain/kotlin/com/nekzabirov/navigatio/common/host/NavHost.kt
+++ src/commonMain/kotlin/com/nekzabirov/navigatio/common/host/NavHost.kt
@@ -1,11 +1,21 @@
 package com.nekzabirov.navigatio.common.host
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.nekzabirov.navigatio.common.state.NavBackState
 import com.nekzabirov.navigatio.common.state.NavigateRoute
@@ -18,20 +28,22 @@ public fun NavHost(
     modifier: Modifier = Modifier.fillMaxSize(),
     navigationController: NavigationController,
     startRoute: String,
-    enterAnimation: EnterTransition = slideInHorizontally(initialOffsetX = { width -> -width }),
-    exitAnimation: ExitTransition = slideOutHorizontally(targetOffsetX = { width -> -width }),
+    enterAnimation: EnterTransition = fadeIn(),
+    exitAnimation: ExitTransition = fadeOut(),
     navigationGraphBuilder: NavigationGraph.() -> Unit
 ): Unit = Box(modifier = modifier.background(MaterialTheme.colorScheme.background)) {
     val navigationGraph = remember(startRoute) {
         NavigationGraph().apply(navigationGraphBuilder)
     }
 
-    val currentNavBackState by remember(startRoute) {
-        navigationController.currentNavBackState
-    }.collectAsState(NavBackState(
-        startRoute,
-        destination = navigationGraph.findDestinations(startRoute)!!
-    ))
+    var currentNavBackState by remember {
+        mutableStateOf(
+            NavBackState(
+                startRoute,
+                destination = navigationGraph.findDestinations(startRoute)!!
+            )
+        )
+    }
 
     AnimatedContent(
         targetState = currentNavBackState,
@@ -42,12 +54,12 @@ public fun NavHost(
             ContentTransform(enter, exit)
         },
         contentKey = { it.destination }
-    ) { it() }
+    ) { it.invoke() }
 
     LaunchedEffect(navigationGraph) {
         navigationController.onNavigateRoute
             .onEach {
-                val currentNavBackState = if (it is NavigateRoute.Destination) {
+                currentNavBackState = if (it is NavigateRoute.Destination) {
                     val parent = if (it.option.launchSingleTop)
                         null
                     else if (it.option.finish)
@@ -75,7 +87,7 @@ public fun NavHost(
                     currentNavBackState.parent!!
                 }
 
-                navigationController._currentNavBackState.send(currentNavBackState)
+                navigationController._currentNavBackState.value = currentNavBackState
             }
             .launchIn(this)
     }
--- src/commonMain/kotlin/com/nekzabirov/navigatio/common/state/NavigationController.kt
+++ src/commonMain/kotlin/com/nekzabirov/navigatio/common/state/NavigationController.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
 @Composable
@@ -13,8 +15,9 @@ public class NavigationController internal constructor(){
     private val _onNavigateRoute = Channel<NavigateRoute>(Channel.BUFFERED)
     internal val onNavigateRoute: Flow<NavigateRoute> = _onNavigateRoute.receiveAsFlow()
 
-    internal val _currentNavBackState = Channel<NavBackState>(Channel.BUFFERED)
-    public val currentNavBackState: Flow<NavBackState> = _currentNavBackState.receiveAsFlow()
+    internal val _currentNavBackState = MutableStateFlow<NavBackState?>(null)
+    public val currentNavBackState: StateFlow<NavBackState?>
+        get() = _currentNavBackState
 
     public fun navigate(route: String, builder: NavigationOptionBuilder.() -> Unit = {}) {
         _onNavigateRoute.trySend(
