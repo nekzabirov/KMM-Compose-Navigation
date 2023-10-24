c70162898ffa5a4fa7064c88895031e9a3be391d
Volodymyr Kryvonos <kruvonosvova@gmail.com>
2023-10-24 17:37:58 +0300

    Fix missing import

--- src/commonMain/kotlin/com/nekzabirov/navigatio/common/store/LocalNavigationStateStore.kt
+++ src/commonMain/kotlin/com/nekzabirov/navigatio/common/store/LocalNavigationStateStore.kt
@@ -2,6 +2,7 @@ package com.nekzabirov.navigatio.common.store
 
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
+import com.nekzabirov.navigatio.common.state.NavigationController
 
-public var LocalNavigationStateStore: ProvidableCompositionLocal<NavigationStateStore> =
+public var LocalNavigationStateStore: ProvidableCompositionLocal<NavigationController.NavigationStateStore> =
     compositionLocalOf { error("No NavigationStateStore found") }
